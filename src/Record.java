b028c1609eb5087cc63771e8a5e5c070ce0ce7bb
Bruno R <shinfujiwara@gmail.com>
2018-04-12 12:34:23 +0100

    Refactoring Record class

--- src/Record.java
+++ src/Record.java
@@ -7,24 +7,57 @@ public class Record {
     private int expenses;
     private int cashflow;
     private String companyName;
+    private List<Ticket> tickets;
 
     public Record(String companyName, List<Ticket> tickets) {
         this.companyName = companyName;
-        this.income = calculateIncome(companyName, tickets);
-        this.expenses = calculateExpenses(companyName, tickets);
-        this.cashflow = calculateCashflow(income,expenses);
+        this.tickets = tickets;
+
+        calculateIncome();
+        calculateExpenses();
+        calculateCashflow();
     }
 
 
-    private int calculateIncome(String companyName, List<Ticket> tickets){
-        return tickets.stream().filter(ticket -> ticket.getSeller().equals(companyName)).mapToInt(Ticket::getTotalCost).sum();
+    private void calculateIncome() {
 
-    }
-    private int calculateExpenses(String companyName, List<Ticket> tickets){
-        return tickets.stream().filter(ticket -> ticket.getBuyer().equals(companyName) ).mapToInt(Ticket::getTotalCost).sum();
-    }
-    private int calculateCashflow(int income, int expenses){
-        return income - expenses;
+        String companyName = getCompanyName();
+        List<Ticket> tickets = getTickets();
+
+        income = tickets.stream().filter(ticket ->
+                ticket.getSeller().equals(companyName)).mapToInt(Ticket::getTotalCost).sum();
     }
 
+    private void calculateExpenses() {
+
+        String companyName = getCompanyName();
+        List<Ticket> tickets = getTickets();
+
+        expenses = tickets.stream().filter(ticket ->
+                ticket.getBuyer().equals(companyName)).mapToInt(Ticket::getTotalCost).sum();
+    }
+
+    private void calculateCashflow() {
+        cashflow = getIncome() - getExpenses();
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public int getExpenses() {
+        return expenses;
+    }
+
+    public int getCashflow() {
+        return cashflow;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    private List<Ticket> getTickets() {
+        return tickets;
+    }
 }

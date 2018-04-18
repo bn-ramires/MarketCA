77fabe18fb6c731aa8849abb903650c7190c531d
Bruno R <shinfujiwara@gmail.com>
2018-04-18 16:30:00 +0100

    Refactoring InputTest.java

---
+++ src/test/java/purchasing/TransactionTest.java
@@ -0,0 +1,69 @@
+package purchasing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class TransactionTest {
+
+    Transaction transactionClass = Transaction.getInstance();
+
+    @Test
+    void makeTransactions() {
+//        transactionClass.makeTransactions();
+
+    }
+
+    @Test
+    void buy() {
+    }
+
+    @Test
+    void isReadyToBuy() {
+    }
+
+    @Test
+    void setBuyingGoal() {
+    }
+
+    @Test
+    void quantityToBuy() {
+    }
+
+    @Test
+    void isReadyToSell() {
+    }
+
+    @Test
+    void getSellers() {
+    }
+
+    @Test
+    void getOriginator() {
+    }
+
+    @Test
+    void getTicketCarer() {
+    }
+
+    @Test
+    void getCurrentBuyerId() {
+    }
+
+    @Test
+    void getCurrentSellerId() {
+    }
+
+    @Test
+    void getMinimumCashAllowance() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void setSellers() {
+    }
+}
